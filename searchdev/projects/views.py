6ceb2064434dbe433a15b50d47a992397cd3fa0e
Briankipkemboi78 <briankipkemboi47@gmail.com>
2025-01-15 13:50:32 +0300

    Adding urls

---
+++ searchdev/projects/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def projects(request):
+    return HttpResponse("Projects Page")
+
+def project(request, pk):
+    return HttpResponse('Single Project' + ' ' + str(pk))
