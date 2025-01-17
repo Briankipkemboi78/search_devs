087b77168aa0de150dc9a92d80f8f30f692eb243
Briankipkemboi78 <briankipkemboi47@gmail.com>
2025-01-17 07:44:59 +0300

    Reorganising the code

--- searchdev/projects/models.py
+++ searchdev/projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
 
+from django.db.models.deletion import CASCADE
+from users.models import Profile
+
+
+
+class Project(models.Model):
+    owner = models.ForeignKey(
+        Profile, null=True, blank=True, on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    feature_image = models.ImageField(
+        null=True, blank=True, default="default.jpg"
+    )
 # Create your models here.
