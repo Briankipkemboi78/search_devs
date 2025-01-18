508cb0ceea54c33b63dd01bff1b2e0eacc1cd63c
Briankipkemboi78 <briankipkemboi47@gmail.com>
2025-01-18 22:58:56 +0300

    Adding files

--- searchdev/projects/models.py
+++ searchdev/projects/models.py
@@ -2,17 +2,17 @@ from django.db import models
 import uuid
 
 from django.db.models.deletion import CASCADE
-from users.models import Profile
+#from users.models import Profile
 
 
 
 class Project(models.Model):
-    owner = models.ForeignKey(
-        Profile, null=True, blank=True, on_delete=models.CASCADE
-    )
+    # owner = models.ForeignKey(
+    #     Profile, null=True, blank=True, on_delete=models.CASCADE
+    # )
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    feature_image = models.ImageField(
-        null=True, blank=True, default="default.jpg"
-    )
+    # feature_image = models.ImageField(
+    #     null=True, blank=True, default="default.jpg"
+    # )
 # Create your models here.
