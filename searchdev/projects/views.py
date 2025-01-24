c0699664b9b47c72ea7766b09ccd19408fef8e80
Briankipkemboi78 <briankipkemboi47@gmail.com>
2025-01-24 12:02:28 +0300

    Instantiating the form

--- searchdev/projects/views.py
+++ searchdev/projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Project
+from .models import Project, Tag
+from .forms import ProjectForm, ReviewForm
 
 
 def projects(request): 
@@ -14,6 +15,22 @@ def project(request, pk):
     return render(request,'projects/single-project.html' )
 
 def createProject(request):
-    
+    profile = request.user.profile
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        newtags = request.POST.get('newtags').replace(',', " ").split()
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
+
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name=tag)
+                project.tags.add(tag)
+            return redirect('account')
+
+
     context = {}
     return render(request, "projects/project_form.html", context)
