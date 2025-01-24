f3c6c9d060a9552c574200f3a331d6670d0e89e9
Briankipkemboi78 <briankipkemboi47@gmail.com>
2025-01-24 19:11:28 +0300

    adding owner

--- searchdev/projects/views.py
+++ searchdev/projects/views.py
@@ -1,25 +1,48 @@
+from django.core import paginator
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Project, Tag
 from .forms import ProjectForm, ReviewForm
+from .utils import searchProjects, paginateProjects
 
 
-def projects(request): 
-    projects = Project.objects.all()
-    context = { 'projects': projects}
+def projects(request):
+    projects, search_query = searchProjects(request)
+    custom_range, projects = paginateProjects(request, projects, 6)
+
+    context = {'projects': projects,
+               'search_query': search_query, 'custom_range': custom_range}
     return render(request, 'projects/projects.html', context)
 
+
 def project(request, pk):
-    projectObject = Project.objects.get(id=pk)
+    projectObj = Project.objects.get(id=pk)
+    form = ReviewForm()
 
-    return render(request,'projects/single-project.html' )
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save()
 
+        projectObj.getVoteCount
+
+        messages.success(request, 'Your review was successfully submitted!')
+        return redirect('project', pk=projectObj.id)
+
+    return render(request, 'projects/single-project.html', {'project': projectObj, 'form': form})
+
+
+@login_required(login_url="login")
 def createProject(request):
     profile = request.user.profile
     form = ProjectForm()
 
     if request.method == 'POST':
-        newtags = request.POST.get('newtags').replace(',', " ").split()
+        newtags = request.POST.get('newtags').replace(',',  " ").split()
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
             project = form.save(commit=False)
@@ -31,10 +54,11 @@ def createProject(request):
                 project.tags.add(tag)
             return redirect('account')
 
-
-    context = {}
+    context = {'form': form}
     return render(request, "projects/project_form.html", context)
 
+
+@login_required(login_url="login")
 def updateProject(request, pk):
     profile = request.user.profile
     project = profile.project_set.get(id=pk)
@@ -56,6 +80,7 @@ def updateProject(request, pk):
     return render(request, "projects/project_form.html", context)
 
 
+@login_required(login_url="login")
 def deleteProject(request, pk):
     profile = request.user.profile
     project = profile.project_set.get(id=pk)
